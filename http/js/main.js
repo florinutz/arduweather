60336e8713b591a2670d30b2f8f908a66ba54b46
Florin Ercuș <florin.ercus@lendico.de>
2016-09-04 12:25:49 +0200

    web app

--- http/js/main.js
+++ http/js/main.js
@@ -8,11 +8,17 @@ $(function() {
                 $('#tat-top > span').html(data.today_average_temperature);
                 $('#yagh-top > span').html(data.yesterday_average_ground_humidity);
                 $('#yat-top > span').html(data.yesterday_average_temperature);
+
                 if (data.advice) {
                     $('#advice').html('Please water plants.');
                 } else {
                     $('#advice').html('Not watering needed.');
                 }
+
+                $('#last-light > div').html(data.last_light.val);
+                $('#last-ground-humidity > div').html(data.last_ground_humidity.val);
+                $('#last-air-humidity > div').html(data.last_air_humidity.val);
+                $('#last-temperature > div > span').html(data.last_temperature.val);
             }
         });
     }, $('body').data('refresh'));
